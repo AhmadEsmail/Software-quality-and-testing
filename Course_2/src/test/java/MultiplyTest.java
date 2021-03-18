6a353280f19a3c7d7af15466b5031c8df1dab774
AhmadEsmail <72101415+AhmadEsmail@users.noreply.github.com>
2021-03-18 23:53:23 +0100

    Update and rename MultiplyTest to MultiplyTest.java

--- Course_2/src/test/java/MultiplyTest.java
+++ Course_2/src/test/java/MultiplyTest.java
@@ -15,8 +15,9 @@ public class MultiplyTest {
 
     @Test
     public void baseMultiplicationTests() {
-        assertEquals(  "10 , 1 must be 10", 11, muxnum.multiply(10, 1) );
-        assertEquals(  "0 , 10 must be 10", 0, muxnum.multiply(0, 10) );
-        assertEquals( "0 , 0 must be 0", 0, muxnum.multiply(0, 0) );
+        assertEquals(  "10 , 1 must be 10", 11, muxnum.mux(10, 1) );
+        assertEquals(  "0 , 10 must be 10", 10, muxnum.mux(0, 10) );
+        assertEquals( "0 , 0 must be 0", 0, muxnum.mux(0, 0) );
+        assertEquals( "8 , 9 must be 9", 9, muxnum.mux(8, 9) );
     }
 }
